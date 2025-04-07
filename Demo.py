246fc270013df1968d210649638a526fdae48c06
Bryan Eduardo Niño Contreras <bryan.ninocn@uanl.edu.mx>
2025-04-07 15:48:49 -0600

    prueba clase cuadrado

--- Demo.py
+++ Demo.py
@@ -2,6 +2,11 @@ class Cuadrado:
     def __init__(self,lado):
         self.lado = lado
         
-    def Area(self):
+    def area(self):
         return self.lado*self.lado
-    
+
+##################################################
+lado = int(input("Introduzca lado del cuadrado: "))
+mi_ejemplo = Cuadrado(lado)
+r = mi_ejemplo.area()
+print(f"Mi area es {r}")
